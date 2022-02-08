a205618521dc7f94544db67cee3440dcd17a8c8e
Anchal-m <97974177+Anchal-m@users.noreply.github.com>
2022-02-08 02:23:27 -0700

    Add files via upload

---
+++ assign24.py
@@ -0,0 +1,4 @@
+
+import pandas as pd
+dataset=pd.read_csv("C:/Users/ADMIN/Desktop/project/play_tennis.csv")
+print(dataset)
---
+++ first.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x=[24,25,26]
+y=[23,24,25]
+plt.plot(x,y)
+plt.show()
